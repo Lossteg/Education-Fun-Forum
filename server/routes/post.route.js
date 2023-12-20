5e58978c026f61f95a224a051b6f5b2b288403c7
VladPristavkin <138557453+VladPristavkin@users.noreply.github.com>
2023-12-20 15:13:51 +0300

    feat: add post validator

---
+++ server/routes/post.route.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const passport = require("passport");
+
+const {
+  getPublicPosts,
+  getPosts,
+  getPost,
+  createPost,
+  confirmPost,
+  rejectPost,
+  deletePost,
+  getCommunityPosts,
+  getFollowingUsersPosts,
+  likePost,
+  unlikePost,
+  addComment,
+  savePost,
+  unsavePost,
+  getSavedPosts,
+  clearPendingPosts,
+} = require("../controllers/post.controller");
+const {
+  postValidator,
+  commentValidator,
+  validatorHandler,
+} = require("../middlewares/post/userInputValidator");
