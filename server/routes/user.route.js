bf0e8457ea2ab11b73ba91f01b8618d074018659
VladPristavkin <138557453+VladPristavkin@users.noreply.github.com>
2023-12-20 14:59:39 +0300

    feat: add avatar upload middleware

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -25,3 +25,13 @@ const {
   addUserValidator,
   addUserValidatorHandler,
 } = require("../middlewares/users/usersValidator");
+
+const { sendVerificationEmail } = require("../middlewares/users/verifyEmail");
+const {
+  sendLoginVerificationEmail,
+} = require("../middlewares/users/verifyLogin");
+
+const avatarUpload = require("../middlewares/users/avatarUpload");
+
+const decodeToken = require("../middlewares/auth/decodeToken");
+const requireAuth = passport.authenticate("jwt", { session: false }, null);
