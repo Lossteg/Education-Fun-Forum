a5307e7348898a9c6a8822ccdc3cc6bac5e6c225
VladPristavkin <94453559+Lossteg@users.noreply.github.com>
2023-12-23 23:39:59 +0300

    feat: add user state module

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 
 import authReducer from "./auth";
 import adminReducer from "./admin";
+import userReducer from "./user";
 import postsReducer from "./posts";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     admin: adminReducer,
+    user: userReducer,
     posts: postsReducer,
 });
 
