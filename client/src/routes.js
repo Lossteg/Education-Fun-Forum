4a83571306cd2d272bb40c8edee76bc057257dce
VladPristavkin <94453559+Lossteg@users.noreply.github.com>
2023-12-23 22:53:16 +0300

    feat: add routes for verifiaction pages

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,4 +1,10 @@
+import { lazy } from "react";
+
 import SignUp from "./pages/SignUp";
+
+const VerifyEmail = lazy(() => import("./pages/VerifyEmail"));
+const EmailVerifiedMessage = lazy(() => import("./pages/EmailVerifiedMessage"));
+const LoginVerified = lazy(() => import("./pages/LoginVerified"));
 
 export const privateRoutes = [
 
@@ -9,4 +15,16 @@ export const publicRoutes = [
       path: "/signup",
       element: <SignUp />,
     },
+    {
+        path: "/auth/verify",
+        element: <VerifyEmail />,
+      },
+      {
+        path: "/email-verified",
+        element: <EmailVerifiedMessage />,
+      },
+      {
+        path: "/verify-login",
+        element: <LoginVerified />,
+      },
 ];
