7481dfe0494e034820b24024dbe94ea0879ae45d
VladPristavkin <94453559+Lossteg@users.noreply.github.com>
2023-12-23 23:04:36 +0300

    feat: add post state module

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import authReducer from "./auth";
 import adminReducer from "./admin";
+import postsReducer from "./posts";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     admin: adminReducer,
+    posts: postsReducer,
 });
 
 export default rootReducer;
