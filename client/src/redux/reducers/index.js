f09056902df7a701ad741baada3c040176cb45e4
VladPristavkin <94453559+Lossteg@users.noreply.github.com>
2023-12-24 00:07:28 +0300

    feat: add community state module

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -4,12 +4,14 @@ import authReducer from "./auth";
 import adminReducer from "./admin";
 import userReducer from "./user";
 import postsReducer from "./posts";
+import communityReducer from "./community";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     admin: adminReducer,
     user: userReducer,
     posts: postsReducer,
+    community: communityReducer,
 });
 
 export default rootReducer;
